9f65bd61c424821c589a9a0542b8316a6defcb9d
yohan ryuking <yohanryuking@gmail.com>
2024-03-16 06:50:18 +0100

    eventos al 300

--- src/components/cards/Events.jsx
+++ src/components/cards/Events.jsx
@@ -7,15 +7,16 @@ const EventCard = ({ event }) => {
     useEffect(() => {
         const fetchUserId = async () => {
             const user = await supabase.auth.getUser();
-            setUserId(user.id);
+            setUserId(user.data.user.id);
         }
         fetchUserId();
     }, []);
+    const eventName = event.name.replace(/\s/g, '%20');
     return (
         <Card sx={{ width: '100%', height: '400px', position: 'relative' }}>
             <CardMedia
                 sx={{ width: '100%', height: '100%' }}
-                image={`https://duerpqsxmxeokygbzexa.supabase.co/storage/v1/object/public/images/${userId}/events/${event.name}`}
+                image={`https://duerpqsxmxeokygbzexa.supabase.co/storage/v1/object/public/images/${userId}/events/${eventName}`}
                 title="Cover Image"
             />
             <CardContent sx={{ position: 'absolute', bottom: 0, left: 0, right: 0, height: '50%', background: 'linear-gradient(to top, rgba(0,0,0,0.8) 0%, rgba(0,0,0,0) 100%)', display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', padding: 2, color: '#fff', zIndex: 1 }}>
