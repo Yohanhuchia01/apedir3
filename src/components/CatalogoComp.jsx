f6c2d75a3eb5eb12b2f01bc36b6a1f68f43b0c60
Lian23 <liancarloloraperz@gmail.com>
2024-03-20 12:42:25 -0400

    ultimos cambios

--- src/components/CatalogoComp.jsx
+++ src/components/CatalogoComp.jsx
@@ -1,40 +1,71 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Typography, Modal } from '@mui/material';
 import { supabase } from '../services/client';
 import EditCategory from './cards/EditCategory';
+import CreateCategory from './cards/CreateCategory';
 
 const Categories = ({ business }) => {
     const [categories, setCategories] = useState([]);
+    const [open, setOpen] = useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleAddCategory = async (category) => {
+        const { data, error } = await supabase
+            .from('cayegoryProducts')
+            .insert([{ 'nameProduct': category, 'owner': business.name }]);
+
+        if (error) {
+            console.log('Error inserting category:', error);
+        } else {
+            setOpen(false);
+        }
+    };
+
+    const handleDeleteCategory = async (id) => {
+        const { error } = await supabase
+            .from('cayegoryProducts')
+            .delete()
+            .eq('id', id);
+
+            if (error) {
+                console.log('Error deleting category:', error);
+            } else {
+                setCategories(categories.filter(category => category.id !== id));
+            }
+    };
 
     useEffect(() => {
         const fetchCategories = async () => {
             const { data: categories, error } = await supabase
                 .from('cayegoryProducts')
                 .select('*')
-                .eq('owner', business.name); // Asegúrate de cambiar 'business_id' y 'id' por los nombres reales de los campos en tu base de datos
+                .eq('owner', business.name);
 
             if (error) {
                 console.log('Error fetching categories:', error);
             } else {
                 setCategories(categories);
-                console.log(categories)
             }
         };
 
         fetchCategories();
-    }, []);
-
-
+    }, [categories]); // Agrega categories como una dependencia del useEffect
 
     return (
         <Box sx={{ m: 2 }}>
             <Typography variant="h4" sx={{ mb: 2 }}>Categorías</Typography>
             {categories.map((category) => (
-                <Box key={category.id} sx={{ mb: 5, m: 5 }}> {/* Agrega un margen inferior a cada EditCategory */}
-                    <EditCategory category={category} />
+                <Box key={category.id} sx={{ mb: 5, m: 5 }}>
+                    <EditCategory category={category} onDelete={handleDeleteCategory} business={business} />
                 </Box>
             ))}
-            <Button variant="contained" sx={{ mt: 2, bgcolor: 'rgb(20, 20, 20)', color: 'common.white' }}>Añadir categoría</Button>
+            <Button variant="contained" sx={{ mt: 2, bgcolor: 'rgb(20, 20, 20)', color: 'common.white' }} onClick={() => setOpen(true)}>Añadir categoría</Button>
+            <Modal open={open} onClose={handleClose}>
+                <CreateCategory addCategory={handleAddCategory} closeModal={handleClose} />
+            </Modal>
         </Box>
     );
 };
--- src/components/cards/EditCategory.jsx
+++ src/components/cards/EditCategory.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Grid, Typography, IconButton, Button } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Grid, Typography, IconButton, Button, Modal } from '@mui/material';
 import { Edit, Delete } from '@mui/icons-material';
-// import EditProducts from './EditProduct';
+import EditProducts from './EditProduct';
+import CreateProduct from './CreateProducts';
 import { Box, styled } from '@mui/system';
+import { supabase } from '../../services/client';
 
 const Root = styled('div')(({ theme }) => ({
     marginBottom: theme.spacing(2),
@@ -30,7 +32,98 @@ const AddButton = styled(Button)(({ theme }) => ({
     marginBottom: theme.spacing(1),
 }));
 
-const EditCategory = ({ category }) => {
+const EditCategory = ({ category, onDelete, business }) => {
+    const [products, setProducts] = useState([]);
+    const [openModal, setOpenModal] = useState(false);
+
+    const handleOpenModal = () => {
+        setOpenModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    };
+
+    const handleAddProduct = async (product) => {
+        const { data, error } = await supabase
+            .from('products')
+            .insert([{ 
+                'name': product.name, 
+                'owner': business.name, 
+                'category': category.id,
+                'provincia': business.provincia,
+                'precio':product.price
+            }]);
+
+        if (error) {
+            console.log('Error inserting product:', error);
+        } else {
+            // console.log(product.image)
+            setProducts([...products, data]);
+            // handleCloseModal();
+             // Si la creación del producto fue exitosa, sube la imagen al bucket
+             if (product.image) {
+                const filePath = `${business.name}/${product.name}.jpg`;
+
+                const { data: uploadData, error: uploadError } = await supabase
+                    .storage
+                    .from('feedImages')
+                    .upload(filePath, product.image, { upsert: true });
+
+                if (uploadError) {
+                    console.error('Error uploading image:', uploadError.message);
+                    toast.error('Error uploading image: ' + uploadError.message);
+                } else {
+                    console.log('Image uploaded successfully:', uploadData);
+                    toast.success('Image uploaded successfully');
+
+                    // Obtén la URL de la imagen
+                    const { data: urlData, error: urlError } = await supabase
+                        .storage
+                        .from('feedImages')
+                        .getPublicUrl(filePath);
+
+                    if (urlError) {
+                        console.error('Error getting image URL:', urlError.message);
+                        toast.error('Error getting image URL: ' + urlError.message);
+                    } else {
+                        // Actualiza el evento con la URL de la imagen
+
+                        const { data, error: updateError } = await supabase
+                            .from('products')
+                            .update({ 'image_url': urlData.publicUrl })
+                            .eq('name', product.name)
+                            .select()
+                        if (updateError) {
+                            console.error('Error updating event:', updateError.message);
+                            // toast.error('Error updating event: ' + updateError.message);
+                        } else {
+                            console.log('Event updated successfully with image URL');
+                            // toast.success('Event updated successfully with image URL');
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data: products, error } = await supabase
+                .from('products')
+                .select('*')
+                .eq('category', category.id); // Busca los productos que pertenecen a la categoría actual
+
+            if (error) {
+                console.log('Error fetching products:', error);
+            } else {
+                setProducts(products); // Guarda los productos en el estado
+            }
+        };
+
+        fetchProducts();
+    }, [category.id]); // Dependencia de useEffect
+
     return (
         <Root>
             <Grid container spacing={2}>
@@ -43,20 +136,23 @@ const EditCategory = ({ category }) => {
                             <Edit />
                         </IconButton>
                     </Box>
-                    <IconButton>
+                    <IconButton onClick={() => onDelete(category.id)}>
                         <Delete color="error" />
                     </IconButton>
 
                 </CategoryRow>
                 <Grid item xs={12}>
-                    <AddButton variant="contained">
+                    <AddButton variant="contained" onClick={handleOpenModal}>
                         Agregar productos
                     </AddButton>
                 </Grid>
                 <Grid item xs={12}>
-                    {/* <EditProducts /> */}
+                    {/* <EditProducts products={products} />  */}
                 </Grid>
             </Grid>
+            <Modal open={openModal} onClose={handleCloseModal}>
+                <CreateProduct closeModal={handleCloseModal} createProduct={handleAddProduct} />
+            </Modal>
         </Root>
     );
 };
